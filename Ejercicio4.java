0850ef64751c0663da23730fa1e2a13f725704e0
Gedy <germangediael-ixcamparijcuc@cunoc.edu.gt>
2021-06-08 23:13:22 -0600

    Ejercicio 4 escrito en java y sigo en prueba de commit

---
+++ Ejercicio4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+	public class Ejercicio4{
+		public static void main(String[] args){
+			Scanner entrada = new Scanner(System.in);
+			int numero = 3;
+			int numero1,numero2;
+			numero1 = numero*numero;
+			System.out.println("El cuadrado del numero es: "+numero1);
+			numero2 = numero*numero*numero;
+			System.out.println("El cubo del numero es: "+ numero2);
+
+		}
+	}
