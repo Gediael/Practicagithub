80f4e5a6b263264169984e30488c6409e27eb818
Gedy <germangediael-ixcamparijcuc@cunoc.edu.gt>
2021-06-08 20:01:34 -0600

    sigo en prueba

---
+++ Ejercicio1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ejercicio1{
+	public static void main(String[] args){
+		Scanner entrada = new Scanner(System.in);
+		int num1, num2, suma;
+		System.out.println("Ingrese un numero");
+		num1  = entrada.nextInt();
+		System.out.println("Ingrese el segundo numero");
+		num2 = entrada.nextInt();
+		suma = num1 + num2;
+		System.out.println("la suma de los numeros es: "+ suma);
+
+
+
+
+
+
+	}
+}
