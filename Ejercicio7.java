28f9243b21ddc3857f1ff4ec220d76544683b0f5
Gedy <germangediael-ixcamparijcuc@cunoc.edu.gt>
2021-06-08 23:47:34 -0600

    ejercicio 7 ejecutado en java ya no puedo mas me duermo jejej

---
+++ Ejercicio7.java
@@ -0,0 +1,12 @@
+public class Ejercicio7{
+	public static void main(String[] args){
+		int aleatorio=10;
+		double disminuido;
+		aleatorio=(int)(Math.random()*50);
+		System.out.println("el numero aleatorio es: "+aleatorio);
+		disminuido = aleatorio - 0.15;
+		System.out.println("El numero disminuido es: " +disminuido);
+	}
+
+
+}
