552a6fb3d32d4a9e132ee1f101770cb1c5c5201d
Gedy <germangediael-ixcamparijcuc@cunoc.edu.gt>
2021-06-08 23:43:33 -0600

    Ejercicio6 ejecutado en java todo funciona bien y tengo sueño

---
+++ Ejercicio6.java
@@ -0,0 +1,12 @@
+public class Ejercicio6{
+	public static void main(String[] args){
+		int aleatorio=0;
+		double aumentado;
+		aleatorio=(int)(Math.random()*200);
+		System.out.println("el numero aleatorio es: "+aleatorio);
+		aumentado = aleatorio+ 0.3;
+		System.out.println("El numero aumentado es: " +aumentado);
+	}
+
+
+}
