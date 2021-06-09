a4c02733c0793396a3dd232bb8faae43fb405032
Gedy <germangediael-ixcamparijcuc@cunoc.edu.gt>
2021-06-09 00:07:46 -0600

    Ejercicio8 ejecutado en java

---
+++ Ejercicio8.java
@@ -0,0 +1,8 @@
+public class Ejercicio8{
+	public static void main(String[] args){
+	int num1=2,num2=3,num3=4;
+	double promedio;
+	promedio = (num1+num2+num3)/3;
+	System.out.println("El promedio de los numeros es: "+ promedio);
+	}
+}
